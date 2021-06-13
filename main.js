139b7d85683943ad25d0ed2b319d604414a2f7a6
Paulo Victor <59893766+pauloprojject@users.noreply.github.com>
2021-06-12 21:42:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css'
 
 import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.129.0-chk6X8RSBl37CcZQlxof/mode=imports/optimized/three.js';
 import { Color, TorusGeometry } from 'https://cdn.skypack.dev/pin/three@v0.129.0-chk6X8RSBl37CcZQlxof/mode=imports/optimized/three.js';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+// import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
